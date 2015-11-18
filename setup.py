0303cfc716fca3c9bc6cdd165bbe801cb22d4c66
Olivia Schaefer <taygetea@gmail.com>
2015-11-18 14:51:23 -0800

    potential

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 
 
 
-setup(name='weather',
+setup(name='termweather',
    version='0.1',
    description='displays the weather in the terminal',
    author='Olivia Schaefer',
    author_email='taygetea@gmail.com',
    url='https://github.com/taygetea/weather',
-   packages=('weather',),
+   packages=('termweather',),
    scripts=(
         'src/bin/weather'
    ),
-   package_dir={'weather':'src/lib'}
+   package_dir={'termweather':'src/lib'}
 )
